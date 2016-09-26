bcf5d5792a5cd23ba33ffbb0e0aa4ae0d9d6c35d
ozthekoder <oozdemir2704@gmail.com>
2016-09-25 23:32:07 -0400

    some work on pieces

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import createStore from './store/createStore'
 import Store from './store'
 import AppContainer from './containers/AppContainer'
 
